08f8220b601b7a8c6059aa915525c5e255885dfc
Eli <elizabeth.aguerrevere@gmail.com>
2023-04-29 11:08:47 -0700

    Initial Commit

---
+++ QuestionsApp/QuestionsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuestionsAppApp.swift
+//  QuestionsApp
+//
+//  Created by Elizabeth Aguerrevere on 4/29/23.
+//
+
+import SwiftUI
+
+@main
+struct QuestionsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
